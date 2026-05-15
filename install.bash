154b4247e18d088ceb0e1d0c182b2045fcf6f5f9
hezhuozhuo <421045249@qq.com>
2026-05-15 17:23:34 +0800

    feat: v1.2.0

--- install.bash
+++ install.bash
@@ -199,9 +199,9 @@ resolve_version() {
                 | grep '"tag_name"' | head -1 \
                 | sed 's/.*"tag_name": *"\([^"]*\)".*/\1/' || true)
         fi
-        VERSION="${VERSION:-v1.1.15}"
+        VERSION="${VERSION:-v1.2.0}"
         if [ -z "$VERSION" ]; then
-            VERSION="v1.1.15"
+            VERSION="v1.2.0"
         fi
         info "最新版本: $VERSION"
     fi
--- packaging/windows/ThingsPanel-Setup.iss
+++ packaging/windows/ThingsPanel-Setup.iss
@@ -3,7 +3,7 @@
 #define AppName "ThingsPanel"
 #define AppVersion GetEnv('TP_VERSION')
 #if AppVersion == ""
-  #define AppVersion "v1.1.15"
+  #define AppVersion "v1.2.0"
 #endif
 #define AppPublisher "ThingsPanel Community"
 #define AppURL "https://thingspanel.io"
